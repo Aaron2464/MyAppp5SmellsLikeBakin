869232405f5acf4b185030802803d62ee1fc9c48
Aaron <tim2464@yahoo.com.tw>
2018-03-06 12:10:11 +0800

    initialize a Fragment with an initial value

--- app/src/main/java/com/example/aaron/myappp5smellslikebakin/ListFragment.java
+++ app/src/main/java/com/example/aaron/myappp5smellslikebakin/ListFragment.java
@@ -1,11 +1,10 @@
 package com.example.aaron.myappp5smellslikebakin;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,7 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         OnRecipeSelectedInterface listener = (OnRecipeSelectedInterface) getActivity();
-        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, true);
 
         RecyclerView recyclerView =(RecyclerView) view.findViewById(R.id.listRecyclerView);
         ListAdapter listAdapter = new ListAdapter(listener);
